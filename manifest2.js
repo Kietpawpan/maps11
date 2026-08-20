79f0cb7bbfb5b4b11cbbc11dd8d5594b3ccec3ca
Monte Kietpawpan <kietpawpan@gmail.com>
2026-08-21 01:33:45 +0700

    Create manifest2.js

---
+++ manifest2.js
@@ -0,0 +1,20 @@
+{
+  "name": "MAPS 11 Pro",
+  "short_name": "MAPS 11",
+  "start_url": "./landfillFireRisk.html",
+  "display": "standalone",
+  "background_color": "#f3f4f6",
+  "theme_color": "#6b21a8",
+  "icons": [
+    {
+      "src": "img/EPO11App.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "img/EPO11App.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
